e2ba40ea27dcc41e5bfca63663aec0e5f703394e
newbeginning <73279725+epiphany97@users.noreply.github.com>
2024-03-15 12:04:38 +0800

    Update deployment.sh

--- RAG-EVA-ON-EKS/deployment-lambda/lambda-bd-knowledgebase/deployment.sh
+++ RAG-EVA-ON-EKS/deployment-lambda/lambda-bd-knowledgebase/deployment.sh
@@ -1,4 +1,6 @@
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 682024804674.dkr.ecr.us-west-2.amazonaws.com
+account_id=$(aws sts get-caller-identity --query Account --output text)
+# echo "$account_id"
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $account_id.dkr.ecr.us-west-2.amazonaws.com
 docker build --platform linux/amd64 -t knowledge-base-lambda-parallel:test .
-docker tag knowledge-base-lambda-parallel:test 682024804674.dkr.ecr.us-west-2.amazonaws.com/knowledge-base:latest
-docker push 682024804674.dkr.ecr.us-west-2.amazonaws.com/knowledge-base:latest
+docker tag knowledge-base-lambda-parallel:test $account_id.dkr.ecr.us-west-2.amazonaws.com/knowledge-base:latest
+docker push $account_id.dkr.ecr.us-west-2.amazonaws.com/knowledge-base:latest
